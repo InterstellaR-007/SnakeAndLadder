01f7eafb0124a420b8f7e198af2e977dcf829333
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-29 02:33:25 +0530

    [Anuj] Two Players

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -6,53 +6,124 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            int die_Roll_Number;
-            int snake_Ladder_Options;
-            int player_Position;
-            int count=0;
+            int next_Chance = 0 ;
+            int playerWon = 0;
+            int die_Roll_Number_p1;
+            int snake_Ladder_Options_p1;
+            int player_Position_p1;
+            int count_p1=0;
+            player_Position_p1 = 0;
 
-            player_Position = 0;
+            int die_Roll_Number_p2;
+            int snake_Ladder_Options_p2;
+            int player_Position_p2;
+            int count_p2 = 0;
+            player_Position_p2 = 0;
 
-            while (player_Position != 100)
+            while (player_Position_p1 != 100 && player_Position_p2 != 100)
             {
-
-                Console.WriteLine("\nCurrent Position of Player is : " + player_Position);
                 Random rnd = new Random();
-                die_Roll_Number = rnd.Next(1, 7);
-                count = count + 1;
-                Console.WriteLine("Number getting after Rolling Die is : " + die_Roll_Number);
-                snake_Ladder_Options = rnd.Next(1, 4);
+                if (next_Chance != 1)
+                {
+                    Console.WriteLine("\nCurrent Position of Player1 is : " + player_Position_p1);
 
-                if (snake_Ladder_Options == 1)
-                {
-                    Console.WriteLine("\n No Play \n");
-                }
-                else if (snake_Ladder_Options == 2)
-                {
-                    Console.WriteLine("\n Ladder: Position incremented by " + die_Roll_Number + "\n");
-                    player_Position = player_Position + die_Roll_Number;
-                    if (player_Position > 100)
+                    die_Roll_Number_p1 = rnd.Next(1, 7);
+                    count_p1 = count_p1 + 1;
+                    Console.WriteLine("Number getting after Rolling Die for Player1 is : " + die_Roll_Number_p1);
+                    snake_Ladder_Options_p1 = rnd.Next(1, 4);
+
+                    if (snake_Ladder_Options_p1 == 1)
                     {
-                        player_Position = player_Position - die_Roll_Number;
+                        Console.WriteLine("\n No Play \n");
+                        next_Chance = 0;
                     }
+                    else if (snake_Ladder_Options_p1 == 2)
+                    {
+                        Console.WriteLine("\n Ladder: Position of Player1 incremented by " + die_Roll_Number_p1 + "\n");
+                        player_Position_p1 = player_Position_p1 + die_Roll_Number_p1;
+                        if (player_Position_p1 > 100)
+                        {
+                            player_Position_p1 = player_Position_p1 - die_Roll_Number_p1;
+                        }
+                        next_Chance = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n Snake: Position of Player 1 decremented by " + die_Roll_Number_p1 + "\n");
+                        player_Position_p1 = player_Position_p1 - die_Roll_Number_p1;
+                        if (player_Position_p1 < 0)
+                        {
+                            player_Position_p1 = 0;
+
+                        }
+                        next_Chance = 0;
+                    }
+                    Console.WriteLine("Player Position of Player1 after die rolling is  : " + player_Position_p1);
+                    if (player_Position_p1 == 100)
+                    {
+                        playerWon = 1;
+                        break;
+                    }
+
+
+                    
+
                 }
                 else
+                    next_Chance = 0;
+
+                if (next_Chance != 1)
                 {
-                    Console.WriteLine("\n Snake: Position decremented by " + die_Roll_Number + "\n");
-                    player_Position = player_Position - die_Roll_Number;
-                    if (player_Position < 0)
+                    Console.WriteLine("\nCurrent Position of Player2 is : " + player_Position_p2);
+
+                    die_Roll_Number_p2 = rnd.Next(1, 7);
+                    count_p2 = count_p2 + 1;
+                    Console.WriteLine("Number getting after Rolling Die for Player2 is : " + die_Roll_Number_p2);
+                    snake_Ladder_Options_p2 = rnd.Next(1, 4);
+
+                    if (snake_Ladder_Options_p2 == 1)
                     {
-                        player_Position = 0;
+                        Console.WriteLine("\n No Play \n");
+                        next_Chance = 0;
                     }
+                    else if (snake_Ladder_Options_p2 == 2)
+                    {
+                        Console.WriteLine("\n Ladder: Position of Player2 incremented by " + die_Roll_Number_p2 + "\n");
+                        player_Position_p2 = player_Position_p2 + die_Roll_Number_p2;
+                        if (player_Position_p2 > 100)
+                        {
+                            player_Position_p2 = player_Position_p2 - die_Roll_Number_p2;
+                        }
+                        next_Chance = 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n Snake: Position of Player 2 decremented by " + die_Roll_Number_p2 + "\n");
+                        player_Position_p2 = player_Position_p2 - die_Roll_Number_p2;
+                        if (player_Position_p2 < 0)
+                        {
+                            player_Position_p2 = 0;
+                        }
+                        next_Chance = 0;
+                    }
+
+                    Console.WriteLine("Player Position of Player2 after die rolling is  : " + player_Position_p2);
+                    if (player_Position_p2 == 100)
+                    {
+                        playerWon = 2;
+                        break;
+                    }
+
+                    
                 }
-                
-                
-
-                Console.WriteLine("Player Position after die rolling is  : " + player_Position);
-
+                else
+                    next_Chance = 0;
             }
-                Console.WriteLine("\n Number of Times Dice is rolled   : " + count);
+            Console.WriteLine("Player" + playerWon+" has won the game");
+
+        }
+                
 
         }
     }
-}
+
