f6b3872a6bc2efc55488bc0fc7f18d174e904ffe
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-29 01:15:17 +0530

    [Anuj] Master Branch

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int die_Roll_Number;
+            int snake_Ladder_Options;
+            int player_Position;
+
+
+            player_Position = 0;
+            Console.WriteLine("Current Position of Player is : " + player_Position);
+        }
+    }
+}
