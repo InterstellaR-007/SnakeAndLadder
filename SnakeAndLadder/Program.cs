b7a922b1a3feba36c7743494862868b278465b63
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-29 01:38:43 +0530

    [Anuj] Player Options

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,6 +13,28 @@ namespace SnakeAndLadder
 
             player_Position = 0;
             Console.WriteLine("Current Position of Player is : " + player_Position);
+            Random rnd = new Random();
+            die_Roll_Number = rnd.Next(1, 7);
+            Console.WriteLine("Number getting after Rolling Die is : " + die_Roll_Number);
+            snake_Ladder_Options = rnd.Next(1, 4);
+
+            if (snake_Ladder_Options == 1)
+            {
+                Console.WriteLine("\nNo Play \n");
+            }
+            else if (snake_Ladder_Options == 2)
+            {
+                Console.WriteLine("\nLadder: Position incremented by " + die_Roll_Number + "\n");
+                player_Position = player_Position + die_Roll_Number;
+            }
+            else
+            {
+                Console.WriteLine("\nSnake: Position decremented by " + die_Roll_Number + "\n");
+                player_Position = player_Position - die_Roll_Number;
+            }
+
+            Console.WriteLine("Player Position after die rolling is  : " + player_Position);
+
         }
     }
 }
