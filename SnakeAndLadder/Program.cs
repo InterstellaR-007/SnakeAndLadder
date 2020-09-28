c1655dacd77246a7c27ad5d8c9f5c52a7bcf9804
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-29 01:45:46 +0530

    [Anuj] Player Repeating till 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,29 +12,38 @@ namespace SnakeAndLadder
 
 
             player_Position = 0;
-            Console.WriteLine("Current Position of Player is : " + player_Position);
-            Random rnd = new Random();
-            die_Roll_Number = rnd.Next(1, 7);
-            Console.WriteLine("Number getting after Rolling Die is : " + die_Roll_Number);
-            snake_Ladder_Options = rnd.Next(1, 4);
 
-            if (snake_Ladder_Options == 1)
+            while (player_Position != 100)
             {
-                Console.WriteLine("\nNo Play \n");
-            }
-            else if (snake_Ladder_Options == 2)
-            {
-                Console.WriteLine("\nLadder: Position incremented by " + die_Roll_Number + "\n");
-                player_Position = player_Position + die_Roll_Number;
-            }
-            else
-            {
-                Console.WriteLine("\nSnake: Position decremented by " + die_Roll_Number + "\n");
-                player_Position = player_Position - die_Roll_Number;
-            }
 
-            Console.WriteLine("Player Position after die rolling is  : " + player_Position);
+                Console.WriteLine("\nCurrent Position of Player is : " + player_Position);
+                Random rnd = new Random();
+                die_Roll_Number = rnd.Next(1, 7);
+                Console.WriteLine("Number getting after Rolling Die is : " + die_Roll_Number);
+                snake_Ladder_Options = rnd.Next(1, 4);
 
+                if (snake_Ladder_Options == 1)
+                {
+                    Console.WriteLine("\n No Play \n");
+                }
+                else if (snake_Ladder_Options == 2)
+                {
+                    Console.WriteLine("\n Ladder: Position incremented by " + die_Roll_Number + "\n");
+                    player_Position = player_Position + die_Roll_Number;
+                }
+                else
+                {
+                    Console.WriteLine("\n Snake: Position decremented by " + die_Roll_Number + "\n");
+                    player_Position = player_Position - die_Roll_Number;
+                }
+                if (player_Position < 0)
+                {
+                    player_Position = 0;
+                }
+
+                Console.WriteLine("Player Position after die rolling is  : " + player_Position);
+
+            }
         }
     }
 }
